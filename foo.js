d23bbb76d0fd6717aeb7d120a867a468881e005d
GitHub_aoi <64397145+GitHub-Aoi@users.noreply.github.com>
2020-05-01 12:47:47 +0900

    initial_commit_
    JavaScriptの練習です。

---
+++ foo.js
@@ -0,0 +1,19 @@
+let youtuber = {
+  list: {
+    business:{
+      youtuber:[
+        {name:"しまぶー", age: 20, teachProgramming() {} },
+        {name:"あっちゃん", age: 37, teachHistry() {} }
+      ],
+      teach() {}
+    },
+    entertainment:{
+      youtuber:[{ name:"ヒカキン" },{name:"はじめしゃちょー"}],
+      makeSmile(){}
+    }
+  },
+  plan(){},
+  uploadVideo(){}
+};
+
+export default youtuber;
